78f74083c658cbdeee5f2f6d65d893dda6245c61
iamargentum <rajat.rj97@gmail.com>
2024-07-27 21:47:28 +0530

    add airdrop command comments

--- src/command.ts
+++ src/command.ts
@@ -1,7 +1,7 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { generate, get_pub_key_from_secret } from './keypair';
-import { transact } from './transaction';
+import { request_airdrop, transact } from './transaction';
 
 const get_secret_key_from_string = (secret_key_string: string) => {
     return secret_key_string
@@ -12,6 +12,28 @@ const get_secret_key_from_string = (secret_key_string: string) => {
 }
 
 yargs(hideBin(process.argv))
+    // request airdrop command
+    .command(
+        'request-airdrop',
+        'Requests an airdrop for given wallet address',
+        (yargs) => yargs
+        .option('wallet-address', {
+            alias: 'w',
+            type: 'string',
+            description: `Requester's wallet address`
+        })
+        .option('sols', {
+            alias: 's',
+            type: 'number',
+            description: 'Number of sols to be transfered'
+        })
+        .demandOption(['wallet-address'], 'Please provide wallet address/public key of the requester')
+        .demandOption(['sols'], 'Specify amount to be transfered'),
+        (argv) => {
+            request_airdrop(argv['wallet-address'], argv.sols);
+        }
+    )
+    // transfer command
     .command(
         'transfer',
         'Creates a transfer between provided wallet addresses',
@@ -43,6 +65,7 @@ yargs(hideBin(process.argv))
             transact(payer_secret_key, argv.to, argv.sols);
         }
     )
+    // keypair commands
     .command(
         'keypair',
         'Creates a new keypair or helps getting a public key from secret key',
--- src/transaction.ts
+++ src/transaction.ts
@@ -1,15 +1,18 @@
 import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
 
-// const CONNECTION_URL = '';
-const CONNECTION_URL = 'http://127.0.0.1:8899';
+const CONNECTION_URL = 'https://api.devnet.solana.com';  // for testing on devnet
+// const CONNECTION_URL = 'http://127.0.0.1:8899'; // for testing on localnet
+
+// create connection
+const connection = new Connection(CONNECTION_URL);
 
 export async function transact(payer_secret: Array<number>, payee_address: string, num_sols: number) {
-    const connection = new Connection(CONNECTION_URL);
 
     // create payer's keypair
     const payer_key_pair = Keypair.fromSecretKey(Uint8Array.from(payer_secret));
     console.log("Payer's public key is - ", payer_key_pair.publicKey.toBase58());
     
+    // create transaction
     const transaction = new Transaction()
         .add(
             SystemProgram.transfer({
@@ -23,7 +26,14 @@ export async function transact(payer_secret: Array<number>, payee_address: strin
     transaction.feePayer = payer_key_pair.publicKey;
     transaction.partialSign(payer_key_pair);
 
+    // send
     const serialized_transaction = transaction.serialize();
     const signature = await connection.sendRawTransaction(serialized_transaction);
     console.log("Transfer successful\nTransfer signature - ", signature);   
+}
+
+export async function request_airdrop(pub_key: string, num_sols: number) {
+    const transaction_id = await connection.requestAirdrop(new PublicKey(pub_key), LAMPORTS_PER_SOL * num_sols);
+    console.log("Request successful\nTransaction id - ", transaction_id);
+    
 }
